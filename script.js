5ef6a2b6289bb35c8f0b8775a431c24ffeaf479e
Valentyn Kyianitsya <valentine1004@ukr.net>
2018-10-09 23:34:13 +0300

    add image

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var road = new Image();
+var car1 = new Image();
+var car2 = new Image();
+var car3 = new Image();
 var auto = new Image();
 
 
 road.src = "img/road.jpg";
+car1.src = "img/car1.png";
+car2.src = "img/car2.png";
+car3.src = "img/car3.png";
 auto.src = "img/auto.png";
+
+//Создание других машинок
+var cars = [];
+
+cars[0] = {
+    x: 0,
+    y: 20
+}
 
 //Position of auto
 var xPos = 200;
@@ -37,8 +51,15 @@ function moveAuto(e){
 
 function draw(){
     ctx.drawImage(road, 0, 0);
+    for(var i = 0; i < cars.length; i++){
+        ctx.drawImage(car1, cars[i].x, cars[i].y, 30, 50);
+        ctx.drawImage(car2, 400, 200, 30, 50);
+        ctx.drawImage(car3, 250, 260, 30, 50);
+        cars[i].y++;
+    }
+    
     ctx.drawImage(auto, xPos, yPos, 30, 50);
-    yPos -= startSpeed;
+    // yPos -= startSpeed;
     requestAnimationFrame(draw);
 };
 
